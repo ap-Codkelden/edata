a44f6dc03e47250cc993193519d48988894553dc
Renat Nasridinov <rino@MacBook-Air.local>
2022-07-30 14:52:32 +0300

    Add verbose parameter, end_date argument

--- extractor.py
+++ extractor.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+
 import edata
-from collections import namedtuple
+import sys
 import argparse
-from datetime import timedelta, date, datetime
 import time
-from pathlib import Path
 import os.path
+from collections import namedtuple
+from datetime import timedelta, date, datetime
+from pathlib import Path
 
 
 Namespace = namedtuple('Namespace', "csv,indent,json,keep_json,lastload,"
@@ -25,37 +27,44 @@ def daterange(start_date, end_date):
             print(isodate)
         yield k.weekday(), isodate
 
-def extract(start_date, end_date):
+def extract(start_date, end_date, verbose=None):
     for single_date in daterange(start_date, end_date):
         weekday, tr_date = single_date
-        if weekday >= 5:
-            continue
-        # print(tr_date)
-        results = Namespace(ascii=False, csv=False,
-                        startdate=tr_date, 
-                        enddate=tr_date,
-                        indent=0, json=False, keep_json=False, lastload=False,
-                        payers=[], ping=False, receipts=[], sqlite=False,
-                        subparser_name='transactions', top100=False, treasury=[],
-                        verbose=False, zipname=os.path.join("data", tr_date))
+        # if weekday >= 5:
+        #     continue
+        if verbose:
+            print(tr_date)
+        results = Namespace(
+            ascii=False, csv=False,
+            startdate=tr_date,
+            enddate=tr_date,
+            indent=0, json=False, keep_json=False, lastload=False,
+            payers=[], ping=False, receipts=[], sqlite=False,
+            subparser_name='transactions', top100=False, treasury=[],
+            verbose=False, zipname=os.path.join("data", tr_date))
         edata.transactions(results)
         time.sleep(1.5)
 
 
-arg_parser = argparse.ArgumentParser()
-arg_parser.add_argument('start_date', type=str,
-                        help='початкова дата завантаження')
-args = arg_parser.parse_args()
-
-try:
-    k = datetime.strptime(args.start_date, "%Y-%m-%d")
-except ValueError as e:
-    print(e.args[0],
-          "Невірний формат дати, треба ISO 8601")
-    sys.exit(1)
-else:
-    start_date = k.date()
-    end_date = date.today()
-    extract(start_date, end_date)
-
-
+if __name__ == "__main__":
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument('start_date', type=str,
+                            help='початкова дата завантаження')
+    arg_parser.add_argument('-ed', type=str,
+                            help='кінцева дата завантаження. Якщо не вказана, '
+                            'береться сьогодні')
+    arg_parser.add_argument('-v', '--verbose', action="store_true",
+                            help='вивід дат')
+    args = arg_parser.parse_args()
+    # print(args)
+    try:
+        k1 = datetime.strptime(args.start_date, "%Y-%m-%d")
+    except ValueError as e:
+        print(e.args[0],
+              "Невірний формат дати, має бути ISO 8601")
+        sys.exit(1)
+    else:
+        start_date = k1.date()
+        end_date = date.today() if args.ed is None else datetime.strptime(
+            args.ed, "%Y-%m-%d").date() + timedelta(days=1)
+        extract(start_date, end_date, verbose=args.verbose)
