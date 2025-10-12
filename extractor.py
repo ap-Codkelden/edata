67840de8904c426b1f6d9fd8ef9a3cdd911fabc3
Renat <mavladi@gmail.com>
2025-10-12 19:12:12 +0300

    error handling

--- extractor.py
+++ extractor.py
@@ -7,6 +7,7 @@ import sys
 import argparse
 import time
 import os.path
+import tempfile
 from collections import namedtuple
 from calendar import monthrange
 from datetime import timedelta, date, datetime
@@ -18,22 +19,28 @@ Namespace = namedtuple('Namespace', "csv,indent,json,keep_json,lastload,"
     "payers,ping,receipts,sqlite,startdate,enddate,subparser_name,top100,"
     "treasury,verbose,zipname,ascii")
 
-Path("data").mkdir(parents=True, exist_ok=True)
+save_dir_name: str = "data"
 start_date = end_date = None
 
+try:
+    save_dir: Path = Path(tempfile.gettempdir()) / save_dir_name
+    save_dir.mkdir(parents=True, exist_ok=True)
+except Exception as e:
+    save_dir = Path(save_dir_name)
+    Path(save_dir).mkdir(parents=True, exist_ok=True)
+    print("Не вдається створити директорію для збереження даних у \n"
+          "тимчасовій директорії. Створено у поточній.\n")
+
+
 def daterange(start_date: date, end_date: date):
-    for n in range(int((end_date - start_date).days)):
-        k = start_date + timedelta(n)
-        isodate = k.isoformat()
-        if k.day == 1:
-            print(isodate)
-        yield k.weekday(), isodate
+    for n in range((end_date - start_date).days + 1):
+        d = start_date + timedelta(days=n)
+        yield d.weekday(), d.isoformat()
+
 
 def extract(start_date: date, end_date: date, verbose: Optional[bool]=None):
     for single_date in daterange(start_date, end_date):
         weekday, tr_date = single_date
-        # if weekday >= 5:
-        #     continue
         if verbose:
             print(tr_date)
         results = Namespace(
@@ -70,7 +77,7 @@ if __name__ == "__main__":
         if args.ed is None:
             end_date = last_day_date(start_date) 
         else:
-            end_date = datetime.strptime(args.ed, r"%Y-%m-%d").date() + timedelta(days=1)
+            end_date = datetime.strptime(args.ed, r"%Y-%m-%d").date()
     except ValueError as e:
         print(e.args[0], "Невірний формат дати, має бути ISO 8601")
         sys.exit(1)
